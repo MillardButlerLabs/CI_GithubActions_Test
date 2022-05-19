caab69d62e1fb29b23b238c04026ca14f47bd6f9
HuxleyIsMe <huxley.millard@gmail.com>
2022-05-19 17:23:40 +0100

    style preeety

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
        <h1>Rise and sunshine you wonderful sunflower you</h1>
+       <img src="./flower.png" alt="flower" className="sun-flower" />;
       </header>
     </div>
   );
